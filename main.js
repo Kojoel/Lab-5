6aba4bea2ad91f3144193af58ba6d8c78c542cd1
Kojoel <nanaabruquah1s@gmail.com>
2024-06-19 21:21:11 +0000

    Website responsiveness added

---
+++ main.js
@@ -0,0 +1,8 @@
+const adviceId = document.getElementById('advice-id');
+const advice = document.getElementById('advice');
+const diceContainer = document.querySelector('.dice-container');
+
+console.log(adviceId)
+console.log(advice)
+console.log(diceContainer);
+
