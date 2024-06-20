0c6ffb80091433846fafa947e3c34580b773b73b
IT Compliance <compliance@amalitech.org>
2024-06-20 02:38:07 -0700

    Cleaning code

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-/*
-const adviceId = document.getElementById('advice-id');
-const advice = document.getElementById('advice');
-const diceContainer = document.querySelector('.dice-container');
-
-// console.log(adviceId)
-// console.log(advice)
-// console.log(diceContainer);
-
-const url = "https://api.adviceslip.com/advice";
-
-async function getData() {
-    try{
-        const response = await fetch(url);
-        if(response.status === 200) {
-            const data = await response.json();
-            console.log('Success:', data);
-            advice.textContent = data.slip.advice;
-            adviceId.textContent = `ADVICE ${data.slip.id}`
-        }else{
-            console.log('Server Error: ', response);
-        }
-    } catch(error) {
-        console.log('Error: ', error);
-    }
-}
-
-diceContainer.addEventListener('click', getData);
-*/
-
 const adviceId = document.getElementById('advice-id');
 const advice = document.getElementById('advice');
 const diceContainer = document.querySelector('.dice-container');
