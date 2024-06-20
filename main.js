ae71d1a2d725a5b418a6628cdb33ac893f437f7c
IT Compliance <compliance@amalitech.org>
2024-06-20 03:57:32 -0700

    Loading functio implemented

--- main.js
+++ main.js
@@ -13,11 +13,20 @@ async function getData() {
             console.log('Success:', data);
             advice.textContent = data.slip.advice;
             adviceId.textContent = `ADVICE ${data.slip.id}`;
-        } else {
-            console.log('Server Error: ', response);
+        // } else {
+        //     console.log('Server Error: ', response);
         }
     } catch (error) {
         console.log('Error: ', error);
+        adviceId.textContent = '';
+        advice.innerHTML = `<div id="skeleton">
+                        <p class="skeleton__text"></p>
+                        <p class="skeleton__text"></p>
+                        <p class="skeleton__text"></p>
+                    </div>`;
+        setInterval(() => {
+            advice.innerHTML = 'Site cannot be reached';
+        }, 6000)
     }
 }
 
